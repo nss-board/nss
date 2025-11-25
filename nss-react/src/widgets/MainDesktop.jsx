b450298fecc3186deea753a2b47082430ae6a986
dodo07070707 <aa01056328583@gmail.com>
2025-11-25 16:57:39 +0900

    front  main end except footer

--- nss-react/src/widgets/MainDesktop.jsx
+++ nss-react/src/widgets/MainDesktop.jsx
@@ -31,14 +31,25 @@ export default function HeaderDesktop() {
                   <img src="./hebi.jpg" className="content-preview-image" />
                 </div>
                 <div className="content-preview-content-wrapper">
-                  <div className="content-title">지금부터 {item}</div>
-                  <div className="content-desc">
-                    기타 줄이 괜히 혼자 울리는 소리가 날 일으키는 것 같아
-                    오늘따라 불은 꺼져 햇살을 받는 방안이 뭔가 허전해 바람아
-                    기억해 줄래 흥얼거릴 때 이 맘을 바라니까 조금 더 멀리 멀리에
-                    어디든 닿게 내 전부를 전부 다줄게 아무것도 들리지 않더라도
-                    내안에 너를위한 음이 들려 설레서 더 미루기 싫다해도 우선
-                    마이크에만 속삭일게 Ooh-oh, ooh-oh
+                  <div className="content-preview-dividewrapper">
+                    <div className="content-title">지금부터 {item}</div>
+                    <div className="content-desc">
+                      기타 줄이 괜히 혼자 울리는 소리가 날 일으키는 것 같아
+                      오늘따라 불은 꺼져 햇살을 받는 방안이 뭔가 허전해 바람아
+                      기억해 줄래 흥얼거릴 때 이 맘을 바라니까 조금 더 멀리
+                      멀리에 어디든 닿게 내 전부를 전부 다줄게 아무것도 들리지
+                      않더라도 내안에 너를위한 음이 들려 설레서 더 미루기
+                      싫다해도 우선 마이크에만 속삭일게 Ooh-oh, ooh-oh
+                    </div>
+                  </div>
+                  <div className="content-footer-wrapper">
+                    <div className="content-footer-top">
+                      <div className="content-footer-content">
+                        2025.11.25 - 0개의 댓글 - 1개의 좋아요
+                      </div>
+                      <div className="content-footer-content">by d0yxn</div>
+                    </div>
+                    <div className="content-footer-bottomline" />
                   </div>
                 </div>
               </div>
